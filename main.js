0e578e44c9682fdee6fe72c861ae2bf0e24b4ad6
Leandro Tamarindo <tamarindodiasl@gmail.com>
2026-09-04 08:55:13 -0300

    First version
    Added songs, images of things she like.

---
+++ main.js
@@ -0,0 +1,39 @@
+function fugir() {
+            const botao = document.querySelector('.botao-fujao');
+            
+            // Calcula a largura e altura máxima da tela visível menos o tamanho do botão
+            const larguraTela = window.innerWidth - botao.offsetWidth;
+            const alturaTela = window.innerHeight - botao.offsetHeight;
+            
+            // Gera uma posição aleatória baseada no tamanho da tela
+            const novoX = Math.floor(Math.random() * larguraTela);
+            const novoY = Math.floor(Math.random() * alturaTela);
+            
+            // Aplica as novas posições ao botão
+            botao.style.left = novoX + 'px';
+            botao.style.top = novoY + 'px';
+            
+            // Remove o transform inicial para não distorcer a nova posição
+            botao.style.transform = 'none';
+        }
+
+function iniciarMusica() {
+    const musica = document.getElementById('minhaMusica');
+    musica.play().catch(error => {
+        console.log("O navegador barrou o autoplay, aguardando clique real.");
+    });
+    
+    // Remove o evento após tocar a primeira vez
+    document.removeEventListener('click', iniciarMusica);
+}
+document.addEventListener('click', iniciarMusica);
+
+function aumentar() {
+    const img = document.getElementById('minhaImagem');
+    img.style.height = '150px'; // Nova altura ao passar o mouse
+}
+
+function diminuir() {
+    const img = document.getElementById('minhaImagem');
+    img.style.height = '100px'; // Volta à altura original quando o mouse sai
+}
